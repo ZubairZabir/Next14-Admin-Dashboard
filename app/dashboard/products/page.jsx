adbc143a45a95c2f3f282c54d15990f4be2dbb60
Zubair Zabir <zzabir@rollins.edu>
2024-07-26 13:02:25 -0400

    Created users and products in UI

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,9 +1,58 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+
 const ProductPage = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}></div>
+              <Image src="/avatar.png" height={40} width={40} />
+              Laptop
+            </td>
+            <td>
+              The XYZ Laptop combines sleek design with powerful performance,
+              featuring a high-resolution display, fast processor, and
+              long-lasting battery life. Perfect for both work and
+              entertainment, it offers seamless multitasking and immersive
+              visuals.
+            </td>
+            <td>$1000</td>
+            <td>07.26.2024</td>
+            <td>available</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+            </td>
+            <td>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,11 +1,161 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Link from "next/link";
 
 const UserPage = () => {
   return (
-    <div>
-      UserPage
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/avatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Cena
+              </div>
+            </td>
+            <td>johncena@gmail.com</td>
+            <td>07.26.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/avatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Cena
+              </div>
+            </td>
+            <td>johncena@gmail.com</td>
+            <td>07.26.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/avatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Cena
+              </div>
+            </td>
+            <td>johncena@gmail.com</td>
+            <td>07.26.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/avatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Cena
+              </div>
+            </td>
+            <td>johncena@gmail.com</td>
+            <td>07.26.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/avatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Cena
+              </div>
+            </td>
+            <td>johncena@gmail.com</td>
+            <td>07.26.2024</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <button className={`${styles.button} ${styles.view}`}>
+                View
+              </button>
+              <button className={`${styles.button} ${styles.delete}`}>
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
