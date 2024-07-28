0350295009ff2bfa07371217be4c0ea2d0cb1c46
Zubair Zabir <zzabir@rollins.edu>
2024-07-27 22:15:25 -0400

    Created a Login Form

---
+++ app/login/page.jsx
@@ -0,0 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
